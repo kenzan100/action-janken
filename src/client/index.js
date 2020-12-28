5baa3f896bb61799e205136bcc1fdb35bc20dbb4
Yuta Miyama <s04155yo@gmail.com>
2020-12-28 17:04:05 -0500

    Draw ball. Move ball by cursor keys.

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,46 @@
 console.log('Hello');
+
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+let x = 10;
+let y = 10;
+let dx = 0;
+let dy = 0;
+
+function draw_ball(x, y) {
+    ctx.beginPath();
+    ctx.arc(x, y, 20, 0, Math.PI*2, false);
+    ctx.fillStyle = "green";
+    ctx.fill();
+    ctx.closePath();
+}
+
+const inputs = {
+    keyDownActions: {
+        'Left':  () => { dx = -2; dy = 0; },
+        'Right': () => { dx =  2; dy = 0; },
+        'Up':    () => { dy = -2; dx = 0; },
+        'Down':  () => { dy =  2; dx = 0; },
+    },
+
+    keyDownHandler(e) {
+        let val = e.key.replace('Arrow', '');
+        const fn = this.keyDownActions[val];
+        if (typeof fn == 'function') {
+            fn();
+        };
+    },
+};
+
+const renderer = {
+    render() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        x += dx;
+        y += dy;
+        draw_ball(x, y);
+    },
+};
+
+document.addEventListener("keydown", inputs.keyDownHandler.bind(inputs), false);
+setInterval(renderer.render, 1000/60);
